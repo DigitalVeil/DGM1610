ea26e95a7dbc8201383ce6dd8c7d6d62493fddeb
Timothy Perez <tcperez1993@gmail.com>
2019-02-05 23:50:21 -0700

    2D Game Vers. 3 Classes Added (Initial Gator Vector Added!)

---
+++ 1610 2D SideScroller/Assets/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+
+	public string name;
+
+	public Color skinColor;
+
+	public Color eyeColor; 
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 1610 2D SideScroller/Assets/Reptile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reptile : Animal
+{
+	public Color scaleColor;
+
+	public Color teethFangColor; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
