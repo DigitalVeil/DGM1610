e5b94a62775601745f07f48ae1508ed06a473e02
Timothy Perez <tcperez1993@gmail.com>
2019-02-24 20:38:32 -0700

    2D Game Vers, Jump Controller Optimization (Added Double Jump!)

--- 1610 2D SideScroller/Assets/Scripts/JumpCharacter.cs
+++ 1610 2D SideScroller/Assets/Scripts/JumpCharacter.cs
@@ -9,19 +9,28 @@ public class JumpCharacter : MonoBehaviour
 	private Vector2 position;
 	public float Gravity = -9.81f;
 	public float JumpValue = 50;
+	public float JumpCount = 2;
+	private float JumpStart; 
 	// Use this for initialization
 	void Start ()
 	{
+		JumpStart = JumpCount; 
 		controller = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetKey(KeyCode.Space))
+		if (JumpCount > 1 && Input.GetKey(KeyCode.Space))
 		{
 			print("we jumped!");
-			position.y = JumpValue;
+			JumpCount--;
+			position.y = JumpValue * Time.deltaTime;
+		}
+
+		if (controller.isGrounded)
+		{
+			JumpCount = JumpStart; 
 		}
 		position.y -= Gravity * Time.deltaTime;
 		controller.Move(position);
