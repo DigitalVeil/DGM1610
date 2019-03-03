aa7343566ba4a5260d1750eb340b1358742f80a3
Timothy Perez <tcperez1993@gmail.com>
2019-03-02 20:06:59 -0700

    2D Game Vers. Added Conditionals/Character Controller

---
+++ 1610 2D SideScroller/Assets/Scripts/Conditionals.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+
+
+	public bool CanWalk = true;
+	public int Number;
+	public string Password; 
+		
+	private void Update () 
+	{
+		if (!CanWalk)
+		{
+			
+		}
+
+		if (Number >= 10)
+		{
+			print("S Rank!!!!");
+		}
+
+		if (Password == "Dreamless")
+		{
+			print("Correct!");
+		}
+	}
+}
