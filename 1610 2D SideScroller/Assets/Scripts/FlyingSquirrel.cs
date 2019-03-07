f276a2a9defacf8a74033aca71abf8b9e35c627f
Timothy Perez <tcperez1993@gmail.com>
2019-03-06 23:01:57 -0700

    2D Game Vers. Added Flying Squirrel Class

---
+++ 1610 2D SideScroller/Assets/Scripts/FlyingSquirrel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingSquirrel : Mammal {
+	
+	public bool canFall = false;
+	public float flightSpeed = 3.7f; 
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+		GetComponent<SpriteRenderer>().color = skinColor;
+
+		GetComponent<SpriteRenderer>().color = furColor
+
+		GetComponent<SpriteRenderer>().color = eyeColor;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
