5ac41395ebeb0c522a00674f90d6f99004c292d2
Timothy Perez <tcperez1993@gmail.com>
2019-03-08 23:17:00 -0700

    2D Game Vers. Added Turtle Class

---
+++ 1610 2D SideScroller/Assets/Scripts/Turtle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : Reptile {
+
+	public int ScaleCount = 100;
+	public float FallSpeed = 6.7f;
+	public FloatData armor;  
+	public bool CanFall = true; 
+	public Color shellColor;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<SpriteRenderer>().color = skinColor;
+		
+		GetComponent<SpriteRenderer>().color = scaleColor;
+
+		GetComponent<SpriteRenderer>().color = shellColor;
+
+		GetComponent<SpriteRenderer>().color = eyeColor;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
