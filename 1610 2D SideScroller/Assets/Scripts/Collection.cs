b8c1d2aeb3f9f6021e70a27a2accc15507b6be9c
Timothy Perez <tcperez1993@gmail.com>
2019-04-07 22:04:32 -0600

    2D Game Vers. Collection List: If/For Loops

--- 1610 2D SideScroller/Assets/Scripts/Collection.cs
+++ 1610 2D SideScroller/Assets/Scripts/Collection.cs
@@ -5,11 +5,40 @@ using UnityEngine;
 public class Collection : ScriptableObject
 {
 
-    public List<Object> CollectionList; 
+    public List<FloatData> CollectionList; 
     
-    public void Collect(GameObject obj)
+    public void Collect(FloatData obj)
     {
         CollectionList.Add(obj);
     }
-    
+
+    public void CollectionInfo()
+    {
+        foreach (var obj in CollectionList)
+        {
+            Debug.Log(obj);
+        }
+    }
+
+    public void SkillInfo()
+    {
+        foreach (var obj in CollectionList)
+        {
+            if (obj.name == "Burrow"){
+                Debug.Log("Look for Places to Burrow!");
+        }
+    }
+}
+ 
+    public void SkillSlots()
+    {
+        for ( int i = 0; i < CollectionList.Count; i++)
+        {
+            
+            if(CollectionList[i].name == "Skill")
+           {
+                Debug.Log(CollectionList[i]);
+           }
+         }
+     }
 }
