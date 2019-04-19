ae3e271dde946f2fb5a352bb34001b168d4515d5
Timothy Perez <tcperez1993@gmail.com>
2019-04-18 22:21:15 -0600

    2D Game Version Added Speed Animation Script

---
+++ 1610 2D SideScroller/Assets/Scripts/RunAnims.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAnims : MonoBehaviour
+{
+
+    private Animator animate;
+    public float Speed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        animate = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animate.SetFloat("Speed", Speed);
+    }
+}
