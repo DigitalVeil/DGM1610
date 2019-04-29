9747022a88f3e06b60e575fefbb5a218b23b9b27
Tim Perez <tcperez1993@gmail.com>
2019-04-28 22:25:04 -0600

    2D Game Version Optimizing Code

---
+++ 1610 2D SideScroller/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float enemySpeed;
+    public GameObject enemySprite;
+    
+    private bool canFlip = true;
+    private bool facingRight = false;
+    private float faceTime = 5f;
+    private float nextFlip = 0f;
+
+    public float aggroTime;
+    private float startAggro;
+    private bool aggroed;
+    private Rigidbody2D enemyRB;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextFlip)
+        {
+            if (Random.Range(0, 10) >= 6) flipFace();
+            nextFlip = Time.time + faceTime; 
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (facingRight && other.transform.position.x < transform.position.x)
+            {
+                flipFace();
+            } else if (!facingRight && other.transform.position.x > transform.position.x)
+            {
+                flipFace();
+            }
+
+            canFlip = false;
+            aggroed = true;
+            startAggro = Time.time + aggroTime;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (startAggro < Time.time)
+            {
+                if(!facingRight) enemyRB.AddForce(new Vector2(-1, 0) * enemySpeed);
+            } else enemyRB.AddForce(new Vector2(1,0) * enemySpeed);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canFlip = true;
+            aggroed = false; 
+            enemyRB.velocity = new Vector2(0f,0f);
+        }
+    }
+
+    void flipFace()
+    {
+        if (!canFlip) return;
+
+        transform.Rotate(0f, 180f, 0f);
+        facingRight = !facingRight;
+    }
+}
--- 1610 2D SideScroller/Assets/Scripts/ThrowRock.cs
+++ 1610 2D SideScroller/Assets/Scripts/ThrowRock.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class ThrowRock : MonoBehaviour
 {
     public Transform throwPoint;
-    public GameObject RockPrefab; 
+    public GameObject RockPrefab;
+    private float fireRate = 0.5f;
+    private float nextRock = 0f;
 
     // Update is called once per frame
     void Update()
@@ -18,6 +20,13 @@ public class ThrowRock : MonoBehaviour
 
     void Shoot()
     {
+        if (Time.time > nextRock)
+        {
+            nextRock = Time.time+fireRate;
+            
+        }
+        
         Instantiate(RockPrefab, throwPoint.position, throwPoint.rotation);
+        
     }
 }
